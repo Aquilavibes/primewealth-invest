b059c6415b6c64584820b923f142b61963715cd6
Aquilavibes <aquilabrama312@gmail.com>
2024-01-25 11:46:38 +0100

    update on FE

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <header>
+<h1 id='logo'> Nova Tech </h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/#">product</RouterLink>
+        <RouterLink to="/#">pricing</RouterLink>
+        <RouterLink to="/#">resources</RouterLink>
+        
+      </nav>
+<RouterLink to="/#">Login</RouterLink>
+      <button> Try MycaseFree </button>
+  
+</header>
+  <RouterView />
+</template>
+
+
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+
+<style scoped>
+
+button {
+  margin-top: 5px;
+  height: 33px;
+  width: 200px;
+  background-color: blue;
+  border-radius: 20px;
+  color: white;
+  
+}
+
+
+#logo {
+  margin-top: -5px;
+  margin-right: 50px;
+  color: blue;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  display: flex;
+  justify-content: space-evenly; 
+}
+
+nav {
+  font-size: 12px;
+  display: flex;
+  gap: 28px;
+  margin-left: 200px;
+  color: black;
+}
+
+
+
+
+nav a.router-link-exact-active {
+   height: 25px;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: rgba(0,0,0,0.5);
+}
+
+
+nav a {
+  color: black;
+  display: inline-block;
+  font-size: 15px;
+  text-decoration-line: none;
+  margin-top: 300px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+header a {
+  margin-top: 10px;
+  text-decoration-line: none;
+  color: black;
+}
+
+header a.router-link-exact-active:hover {
+  background-color: rgba(0,0,0,0.5);
+  height: 30px;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,81 @@
+<template>
+
+  <div class='homy'>
+
+ <p id='cases'>{{cases}} </p>
+
+<h1 id='intro'>Nova Tech for Easy, P2p Buying <br>and Selling Of goods <br>and services.</h1>
+
+<p id='tiny-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem quis ut<br>Lorem ipsum dolor, sit amet consectetur adipisicing<br>Lorem ipsum dolor, sit amet consectetur quo</p>
+ <br>
+<input type='email' id='newletter' placeholder="Email address"> <button id='send' type='submit'> > </button>
+
+<img src='../assets/man.png' id='first-img'>
+
+</div>
+
+
+
+
+</template>
+
+
+<script>
+
+export default {
+data(){
+  return {
+    cases: 'CASES FOR PEAK EFFICIENCY.',
+     
+  }
+}
+}
+</script>
+
+<style scoped>
+
+#newletter {
+  margin-left: 130px;
+   height: 30px;
+   border-radius: 20px;
+   width: 230px;
+}
+
+
+  
+
+#send {
+  background-color: blue;
+  color: white;
+  margin-left: -35px;
+  border-radius: 80px;
+  font-size: 20px;
+  
+}
+
+
+#first-img {
+  width: 600px;
+  margin-left: 700px;
+  margin-top: -500px;
+}
+
+#tiny-text {
+font-size: 15px;
+margin-left: 130px;
+font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+#intro {
+  margin-left: 130px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 50px;
+}
+
+#cases {
+  color: orange;
+  margin-top: 80px;
+  margin-left: 130px;
+}
+</style>
+
