05f43bd5e8ba98f8e575f3a5c19694353be7581f
Cyber Quils <105954716+Aquilavibes@users.noreply.github.com>
2025-03-29 00:22:00 +0100

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,13 @@
 <template>
-  <Navbar>
+  
+  <Sidebar v-if="isNavOpen" />
+  <div class="min-h-screen  items-center justify-center bg-gray-900 px-4">
+    <Navbar>
     <header class="bg-black text-blue-500 flex items-center justify-between px-4 py-3 shadow-lg">
       <ion-icon class="menu text-3xl cursor-pointer" name="menu-outline" @click="openModal"></ion-icon>
       <ion-icon class="profile text-3xl cursor-pointer" name="person-circle-outline"></ion-icon>
     </header>
   </Navbar>
-  <br>
-  <Sidebar v-if="isNavOpen" />
-  <div class="min-h-screen  items-center justify-center bg-gray-900 px-4">
     <p class="text-[25px] font-mono font-semibold text-blue-500">Welcome, {{ names }} </p>
     <br><br>
     <p id="before-dash" class="text-md text-blue-300 font-sans mb-4">Home > Dashboard > Home</p>
